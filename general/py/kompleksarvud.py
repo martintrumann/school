347a9278e4cffd612b5c320f5bfe7112e8088ac4
Martin Trumann <martin.trumann@gmail.com>
2019-01-29 11:27:38 +0200

    addditions

--- general/py/kompleksarvud.py
+++ general/py/kompleksarvud.py
@@ -2,39 +2,45 @@ import math
 a = int(input("sisesta reaalosa: "))
 b = int(input("sisesta imaginaarosa ilma i-ta: "))
 
-r = a * a  + b * b
+r = a * a + b * b
 if math.sqrt(r) % 1 == 0:
-	print("moodul on", math.sqrt(r))
+	r = round(math.sqrt(r))
+	print("moodul on", r)
 else:
-	print("moodul on \u221a" + str(r))
+	r = "\u221a" + str(r)
+	print("moodul on", r)
 
 if b == 0 and a > 0:
 	fiideg = 0
 elif a == 0 and b > 0:
 	fiideg = 90
-elif b == 10 and a < 0:
+elif b == 0 and a < 0:
 	fiideg = 180
 elif a == 0 and b < 0:
 	fiideg = 270
 else:
 	fiirad = math.atan(b / a)
 	fiideg = math.degrees(fiirad)
-	if fiideg > 360:
+	while fiideg > 360:
 		fiideg = fiideg - 360
-	elif a > 0 and b > 0: # I sector
-		while fiideg > 90 and fiideg < 0:
+	if a > 0 and b > 0: # I sector
+		while fiideg > 90 or fiideg < 0:
 			fiideg = fiideg + 180
 	elif a < 0 and b > 0: # II sector
-		while fiideg > 180 and fiideg < 90:
-			fii = fiideg + 180
+		while fiideg > 180 or fiideg < 90:
+			fiideg = fiideg + 180
 	elif a < 0 and b < 0: # III sector
-		while fiideg > 270 and fiideg < 180:
+		while fiideg > 270 or fiideg < 180:
 			fiideg = fiideg + 180 
 	elif a > 0 and b < 0: # IV sector
-		while fiideg > 360 and fiideg < 270:
+		while fiideg > 360 or fiideg < 270:
 			fiideg = fiideg + 180
 	else:
 		print("how did you get here? it should be impossible")
 
 fiideg = round(fiideg)
 print("fii on", fiideg)
+
+# trigonomeetriline kuju
+
+print(str(r) +  "(cos" + str(fiideg) + "\u00b0 + i * sin" + str(fiideg) + "\u00b0)")
