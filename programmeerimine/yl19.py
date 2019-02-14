541ae5757cecd39626af9472c57071232970d99a
Martin Trumann <martin.trumann@gmail.com>
2019-02-14 09:31:38 +0200

    made it work

--- programmeerimine/yl19.py
+++ programmeerimine/yl19.py
@@ -1,17 +1,21 @@
 import random
 
-game = True
 money = 100
 print("Let's play!")
+game = True
 while game == True:
 	cards = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"]
 
 	pcards = [random.choice(cards), random.choice(cards)]
 	dcards = [random.choice(cards), random.choice(cards)]
 
-	phand = 0
-	dhand = 0
-
+	# Losing conditions
+	if money < 0:
+		print("You're in debt, Good luck paying it off")
+		break
+	elif money == 0:
+		print("You ran out of money, but at least you don't have a debt")
+		break
 	# Bet 
 	print("You have", str(money) + "\u0024")
 	bet = None
@@ -19,6 +23,8 @@ while game == True:
 		bet = input("What's your bet: ")
 		if not bet:
 			print("insert bet")
+		elif bet == "max":
+			bet = money
 	money -= int(bet)
 
 	# Round start
@@ -27,6 +33,10 @@ while game == True:
 	while round == True: 
 		print("Dealers card is", ', '.join(dcards[1]))
 		print("Your cards are", ', '.join(pcards))
+
+		phand = 0
+		dhand = 0
+
 		# Player Choice
 		i = input("Hit, Stand or Leave: ")		
 		if i == "H" or i == "h" or i == "Hit" or i == "hit":
@@ -36,13 +46,12 @@ while game == True:
 			input()
 		elif i == "L" or i == "l" or i == "leave" or i == "leave":
 			print("Good game! You made", str(money) + "\u0024")
-			game = False
-			round = False
+			game == False
+			break
 		else:
 			print("You decided to stand")
 			round = False
 			print("Dealer's cards are", ", ".join(dcards))
-			print("your total was", phand)
 		
 		# players total
 		for c in pcards:
@@ -56,44 +65,36 @@ while game == True:
 				phand += 10
 			else:
 				phand += int(c)
-		print(phand)
+
+		print("your hands total is", phand)
 		if phand > 21:
 			print("You're Bust, Bet's gone")
-			round = False
+			break
 		elif phand == 21:
 			print("Excacly 21, you're lucky")
 		
 		# Dealers hand
-		
-		for c in dcards:
-			if c == "A":
-				continue
-			elif c == "J" or c == "Q" or c == "K":
-				phand += 10
-			else:
-				phand += int(c)
-
-		for c in dcards:
-			if c == "A":
-				if dhand + 11 > 17 and dhand + 11 < 17:
-					dhand += 11
+		if round == False:
+			for c in dcards:
+				if c == "A":
+					continue
+				elif c == "J" or c == "Q" or c == "K":
+					dhand += 10
 				else:
-					dhand += 1
+					dhand += int(c)
+
+			for c in dcards:
+				if c == "A":
+					if dhand + 11 > 17 and dhand + 11 < 21:
+						dhand += 11
+					else:
+						dhand += 1
 
 		# winning calculation
-		if phand > dand:
-			print("you win! Bet's doubled")
-			bet *= 2
-		else:
-			print("You lost!")
-			round = False
-
-		# Losing conditions
-		if money < 0:
-			print("You're in debt, Good luck paying it off")
-			game = False
-			round = False
-		elif money == 0:
-			print("You ran out of money, but at least you don't have a debt")
-			game = False
-			round = False
+		if round == False:
+			if phand > dhand:
+				print("you win! Bet's doubled")
+				money += 2 * int(bet)
+			else:
+				print("You lost!")
+				round = False
